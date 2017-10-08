6bf1d86bc33fc46d4c8b8f6913ba6657895a8c32
Muhamad Fikri Alhawarizmi <mfikria@gmail.com>
2017-10-08 08:06:13 +0700

    Define DetailPage

---
+++ src/routes/DetailPage.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { graphql } from 'react-apollo';
+
+// Helpers
+import { imageUrl } from 'src/helpers/index';
+
+// Components
+import PokemonTypeLabel from 'src/components/PokemonTypeLabel';
+import PokemonTab from 'src/components/PokemonTab/index';
+
+// Style
+import { List, Dimmer, Loader, Segment, Grid, Image, Card, Icon, Tab } from 'semantic-ui-react';
+
+// Query
+import pokemonDetailQuery from 'src/graphql/queries/pokemon_detail.gql';
+
+class DetailPage extends React.PureComponent {
+
+  render() {
+    
+
+    if(pokemon){
+      content = (
+        <Card fluid>
+          <Image src={imageUrl(pokemon.id)} size='medium' centered wrapped />
+          <Card.Content>
+            <Card.Header>{pokemon.name}</Card.Header>
+            <Card.Description>{pokemon.descriptions.edges[0].node.description}</Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            <PokemonTab pokemon={pokemon}/>
+          </Card.Content>
+        </Card>
+      );
+    }
+
+    return (
+      <div>
+        {content}
+      </div>
+    );
+  }
+}
+
+const withData = graphql(pokemonDetailQuery, {
+  options: ({ match }) => ({
+    variables: {id: match.params.id}
+  })
+});
+
+export default withData(DetailPage);
